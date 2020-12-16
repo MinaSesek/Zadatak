cef190c570d6908b94e9dccc143474f9d09cced2
Mina Mina <30478695+minahorvat@users.noreply.github.com>
2020-12-16 10:36:04 +0100

    initial commit

---
+++ vip-task-master/vip-task-master/js/main.js
@@ -0,0 +1,43 @@
+/* Nav toggle */
+$(".hamburger").click(function () {
+    $(this).toggleClass("active");
+    $(".mobile-menu").toggle();
+});
+
+/* Img toggle */
+var img = document.getElementById("img-area");
+
+function black() {
+    img.src = "img/xs-black.png"
+}
+
+function gray() {
+    img.src = "img/xs-gray.png"
+};
+
+function yellow() {
+    img.src = "img/xs-yellow.png"
+};
+/* Select option */
+$("select").change(function () {
+    $(this).find("option:selected").each(function () {
+        var optionValue = $(this).attr("value");
+        if (optionValue) {
+            $(".box").not("." + optionValue).hide();
+            $("." + optionValue).show();
+        } else {
+            $(".box").hide();
+        }
+    });
+}).change();
+
+/* Button scroll */
+$(document).scroll(function () {
+    var y = $(this).scrollTop();
+    if (y > 800) {
+        $('.fixed-button').fadeIn();
+    } else {
+        $('.fixed-button').fadeOut();
+    }
+
+});
